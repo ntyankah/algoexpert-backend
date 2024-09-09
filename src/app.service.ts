e22b055804dbc662d12d8bbccb1c91e9e312171f
Nathaniel <yankahnathanieltopp@gmail.com>
2024-09-09 23:34:43 +0000

    setup config service to read environment variables

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
+
   getHello(): string {
+    const databaseName = this.configService.get<string>('DATABASE_USER')
+    const passWord = this.configService.get<string>('DATABASE_PASSWORD')
+    console.log('name: ', databaseName)
+    console.log('password: ', passWord)
     return 'Hello World!';
   }
 }
