1f2fbdea26cbccd11b6e6286141ddd3d684fe461
Nathaniel <yankahnathanieltopp@gmail.com>
2024-09-11 06:26:33 +0000

    remove logs to test environment variables configuration

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,13 +3,8 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
 
   getHello(): string {
-    const databaseName = this.configService.get<string>('DATABASE_USER')
-    const passWord = this.configService.get<string>('DATABASE_PASSWORD')
-    console.log('name: ', databaseName)
-    console.log('password: ', passWord)
     return 'Hello World!';
   }
 }
